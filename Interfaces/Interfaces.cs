d7dea3d2d13a7cdc9986539b18070e0e681066b2
ISRG <shubhamgiri602@gmail.com>
2025-07-06 22:02:30 +0530

    Update Interfaces.cs

--- Interfaces/Interfaces.cs
+++ Interfaces/Interfaces.cs
@@ -1,1 +1,14 @@
+using System.Collections.Generic;
+using DatabaseManager.Models;
 
+namespace DatabaseManager.Interfaces
+{
+    public interface IUserRepository
+    {
+        void InsertUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+        User GetUserById(int id);
+        List<User> GetAllUsers();
+    }
+}
